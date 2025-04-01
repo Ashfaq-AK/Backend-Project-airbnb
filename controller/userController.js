a87562e146660f45fcb1ff36e86a9c022c974c49
Ashfaq-AK <ashfaqahmedkhan1710@gmail.com>
2025-04-01 22:33:58 +0530

    Logout option added

--- controller/userController.js
+++ controller/userController.js
@@ -1,8 +1,27 @@
 const Home = require('../models/HomeData')
 
+
+exports.userIndex = (req, res, next) => {
+  const regHouse = Home.fetchAll((regHouse) => {
+    res.render('userSide/userIndex', {regHouse: regHouse});
+  });
+  
+}
 exports.userHome = (req, res, next) => {
   const regHouse = Home.fetchAll((regHouse) => {
     res.render('userSide/userHome', {regHouse: regHouse});
+  });
+  
+}
+exports.userBooking = (req, res, next) => {
+  const regHouse = Home.fetchAll((regHouse) => {
+    res.render('userSide/userBooking', {regHouse: regHouse});
+  });
+  
+}
+exports.userFavourite = (req, res, next) => {
+  const regHouse = Home.fetchAll((regHouse) => {
+    res.render('userSide/userFavourite', {regHouse: regHouse});
   });
   
 }
--- router/userRouter.js
+++ router/userRouter.js
@@ -3,7 +3,10 @@ const userRouter = express.Router();
 
 const userController = require('../controller/userController')
 
-userRouter.get('/user', userController.userHome)
+userRouter.get('/user', userController.userIndex)
+userRouter.get('/home', userController.userHome)
+userRouter.get('/bookings', userController.userBooking)
+userRouter.get('/favourite', userController.userFavourite)
 
 
 module.exports = userRouter;
